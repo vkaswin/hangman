ffdfbb897b20d9b6a618e2890a5cb1e043485173
aswin-13282 <aswin-13282@aswin-13282.csez.zohocorpin.com>
2023-01-16 15:06:21 +0530

    update

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:vkaswin/hangman-vue.git master:gh-pages
+git push -f git@github.com:vkaswin/hangman.git master:gh-pages
 
 cd -
